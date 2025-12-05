e4dcd1936fc349166d1ad74e736a7146f681510d
Azra-A <aazra276@gmail.com>
2025-12-05 12:37:20 +0200

    DeletePlayer & List Design

--- FootballManager/UserControls/Players/DeletePlayerControl.Designer.cs
+++ FootballManager/UserControls/Players/DeletePlayerControl.Designer.cs
@@ -28,17 +28,100 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new Label();
+            teamLabel = new Label();
+            fullNameLabel = new Label();
+            teamComboBox = new ComboBox();
+            fullNameComboBox = new ComboBox();
+            addButton = new Button();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 36F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 204);
+            label1.ForeColor = Color.DarkRed;
+            label1.Location = new Point(210, 61);
+            label1.Name = "label1";
+            label1.Size = new Size(411, 81);
+            label1.TabIndex = 25;
+            label1.Text = "Delete Player";
+            // 
+            // teamLabel
+            // 
+            teamLabel.AutoSize = true;
+            teamLabel.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold);
+            teamLabel.ForeColor = Color.DarkRed;
+            teamLabel.Location = new Point(225, 230);
+            teamLabel.Name = "teamLabel";
+            teamLabel.Size = new Size(45, 20);
+            teamLabel.TabIndex = 24;
+            teamLabel.Text = "Team";
+            // 
+            // fullNameLabel
+            // 
+            fullNameLabel.AutoSize = true;
+            fullNameLabel.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold);
+            fullNameLabel.ForeColor = Color.DarkRed;
+            fullNameLabel.Location = new Point(210, 306);
+            fullNameLabel.Name = "fullNameLabel";
+            fullNameLabel.Size = new Size(79, 20);
+            fullNameLabel.TabIndex = 23;
+            fullNameLabel.Text = "Full Name";
+            // 
+            // teamComboBox
+            // 
+            teamComboBox.DropDownStyle = ComboBoxStyle.DropDownList;
+            teamComboBox.FormattingEnabled = true;
+            teamComboBox.Location = new Point(311, 227);
+            teamComboBox.Name = "teamComboBox";
+            teamComboBox.Size = new Size(200, 28);
+            teamComboBox.TabIndex = 22;
+            // 
+            // fullNameComboBox
+            // 
+            fullNameComboBox.DropDownStyle = ComboBoxStyle.DropDownList;
+            fullNameComboBox.FormattingEnabled = true;
+            fullNameComboBox.Location = new Point(311, 303);
+            fullNameComboBox.Name = "fullNameComboBox";
+            fullNameComboBox.Size = new Size(200, 28);
+            fullNameComboBox.TabIndex = 21;
+            // 
+            // addButton
+            // 
+            addButton.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            addButton.ForeColor = Color.DarkRed;
+            addButton.Location = new Point(321, 479);
+            addButton.Name = "addButton";
+            addButton.Size = new Size(115, 40);
+            addButton.TabIndex = 20;
+            addButton.Text = "Delete";
+            addButton.UseVisualStyleBackColor = true;
             // 
             // DeletePlayerControl
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Linen;
+            Controls.Add(label1);
+            Controls.Add(teamLabel);
+            Controls.Add(fullNameLabel);
+            Controls.Add(teamComboBox);
+            Controls.Add(fullNameComboBox);
+            Controls.Add(addButton);
             Name = "DeletePlayerControl";
             Size = new Size(830, 580);
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label teamLabel;
+        private Label fullNameLabel;
+        private ComboBox teamComboBox;
+        private ComboBox fullNameComboBox;
+        private Button addButton;
     }
 }
--- FootballManager/UserControls/Players/ListPlayersControl.Designer.cs
+++ FootballManager/UserControls/Players/ListPlayersControl.Designer.cs
@@ -29,42 +29,111 @@
         private void InitializeComponent()
         {
             components = new System.ComponentModel.Container();
-            dataGridView1 = new DataGridView();
             teamBindingSource = new BindingSource(components);
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            label2 = new Label();
+            label1 = new Label();
+            competitionRadioButton = new RadioButton();
+            countryRadioButton = new RadioButton();
+            teamRadioButton = new RadioButton();
+            dataGridView1 = new DataGridView();
             ((System.ComponentModel.ISupportInitialize)teamBindingSource).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
-            // 
-            // dataGridView1
-            // 
-            dataGridView1.AutoGenerateColumns = false;
-            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.DataSource = teamBindingSource;
-            dataGridView1.Location = new Point(53, 109);
-            dataGridView1.Name = "dataGridView1";
-            dataGridView1.RowHeadersWidth = 51;
-            dataGridView1.Size = new Size(732, 440);
-            dataGridView1.TabIndex = 1;
             // 
             // teamBindingSource
             // 
             teamBindingSource.DataSource = typeof(Models.Team);
             // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(128, 90);
+            label2.Name = "label2";
+            label2.Size = new Size(119, 20);
+            label2.TabIndex = 13;
+            label2.Text = "Show players by:";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 24F, FontStyle.Bold | FontStyle.Italic);
+            label1.Location = new Point(252, 19);
+            label1.Name = "label1";
+            label1.Size = new Size(287, 54);
+            label1.TabIndex = 12;
+            label1.Text = "List of players";
+            // 
+            // competitionRadioButton
+            // 
+            competitionRadioButton.AutoSize = true;
+            competitionRadioButton.Location = new Point(569, 88);
+            competitionRadioButton.Name = "competitionRadioButton";
+            competitionRadioButton.Size = new Size(113, 24);
+            competitionRadioButton.TabIndex = 11;
+            competitionRadioButton.TabStop = true;
+            competitionRadioButton.Text = "Competition";
+            competitionRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // countryRadioButton
+            // 
+            countryRadioButton.AutoSize = true;
+            countryRadioButton.Location = new Point(426, 88);
+            countryRadioButton.Name = "countryRadioButton";
+            countryRadioButton.Size = new Size(81, 24);
+            countryRadioButton.TabIndex = 10;
+            countryRadioButton.TabStop = true;
+            countryRadioButton.Text = "Country";
+            countryRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // teamRadioButton
+            // 
+            teamRadioButton.AutoSize = true;
+            teamRadioButton.Location = new Point(296, 88);
+            teamRadioButton.Name = "teamRadioButton";
+            teamRadioButton.Size = new Size(66, 24);
+            teamRadioButton.TabIndex = 9;
+            teamRadioButton.TabStop = true;
+            teamRadioButton.Text = "Team";
+            teamRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AutoGenerateColumns = false;
+            dataGridView1.BackgroundColor = Color.BurlyWood;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.DataSource = teamBindingSource;
+            dataGridView1.Location = new Point(49, 122);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(732, 440);
+            dataGridView1.TabIndex = 8;
+            // 
             // ListPlayersControl
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Linen;
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(competitionRadioButton);
+            Controls.Add(countryRadioButton);
+            Controls.Add(teamRadioButton);
             Controls.Add(dataGridView1);
             Name = "ListPlayersControl";
             Size = new Size(830, 580);
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ((System.ComponentModel.ISupportInitialize)teamBindingSource).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
-
-        private DataGridView dataGridView1;
         private BindingSource teamBindingSource;
+        private Label label2;
+        private Label label1;
+        private RadioButton competitionRadioButton;
+        private RadioButton countryRadioButton;
+        private RadioButton teamRadioButton;
+        private DataGridView dataGridView1;
     }
 }
